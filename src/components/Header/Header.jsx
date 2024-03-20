d6c8fa29ced269f0401b93991c597f0b8d1a6f4c
maab-fw <maabfw@proton.me>
2024-03-20 14:39:36 +0600

    useNavigation() topic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import "./Header.css"
 const Header = () => {
     return (
@@ -6,7 +6,9 @@ const Header = () => {
             <nav>
                 <span>My Website</span>
                 <Link to="/">Home</Link>
+                <NavLink to="/users">Users (Nav)</NavLink>
                 <Link to="/users">Users</Link>
+                <NavLink to="/posts">Posts (Nav)</NavLink>
                 <Link to="/posts">Posts</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
